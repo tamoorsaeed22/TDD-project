8184383fbeaa30bd93b18cd7bb1d7a437d312bdd
Tamoor saeed <115831124+tamoorsaeed22@users.noreply.github.com>
2023-08-03 14:18:59 +0500

    solver method definition

--- solver.rb
+++ solver.rb
@@ -1,2 +1,13 @@
 class Solver
-    
+    def factrorial(number)
+        return false
+    end
+
+    def reverse(word)
+        false
+    end
+
+    def fizzbuzz(num)
+       false
+    end
+end
--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -1,4 +1,4 @@
-require_relative 'solver'
+require_relative '../solver'
 
 RSpec.describe Solver do
   subject(:solver) { described_class.new }
